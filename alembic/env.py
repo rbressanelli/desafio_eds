420be29de71206a0a033249cb21845c31b7b2b04
Roberto Bressanelli <rbressanelli@gmail.com>
2026-02-11 13:28:41 -0300

    Feat: added database to alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,5 @@
+from core.base import Base
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -18,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
