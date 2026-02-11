38647c2f880c73688bb3dc6b447c8e94554c218b
Roberto Bressanelli <rbressanelli@gmail.com>
2026-02-11 13:33:41 -0300

    Feat: implemented user model

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.base import Base
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement="auto")
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    surname: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    age: Mapped[int] = mapped_column()
