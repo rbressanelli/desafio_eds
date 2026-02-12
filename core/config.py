1257c22133ed5b31c0471180348ac01280933fdd
Roberto Bressanelli <rbressanelli@gmail.com>
2026-02-12 08:32:16 -0300

    Fix: corrected localhost correct url

--- core/config.py
+++ core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     """
 
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:postgres@0.0.0.0:5433/app_db'
+    DB_URL: str = 'postgresql+asyncpg://postgres:postgres@localhost:5433/app_db'
 
 
     class Config:
